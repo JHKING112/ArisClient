e90836fd9e9d997d8d8c0f3907c64fa4559f33f4
JHKING112 <98388174+JHKING112@users.noreply.github.com>
2023-06-09 23:38:26 +0900

    make folder fix

--- ArisMainScript.lua
+++ ArisMainScript.lua
@@ -49,43 +49,46 @@ local function arisGithubRequest(scripturl)
 end
 
 
-local commit = "main"
-for i,v in pairs(game:HttpGet("https://github.com/JHKING112/ArisClient"):split("\n")) do 
+if not shared.ArisDeveloper then 
+	local commit = "main"
+	for i,v in pairs(game:HttpGet("https://github.com/JHKING112/ArisClient"):split("\n")) do 
 		if v:find("commit") and v:find("fragment") then 
-		local str = v:split("/")[5]
-		commit = str:sub(0, str:find('"') - 1)
-		break
+			local str = v:split("/")[5]
+			commit = str:sub(0, str:find('"') - 1)
+			break
+		end
 	end
-end
-if commit then
-	if isfolder("aris") then 
-		if ((not isfile("aris/commithash.txt")) or (readfile("aris/commithash.txt") ~= commit or commit == "main")) then
-			for i,v in pairs({"aris/Universal.lua", "aris/MainScript.lua", "aris/GuiLibrary.lua"}) do 
-				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-					delfile(v)
-				end 
-			end
-			if isfolder("aris/CustomModules") then 
-				for i,v in pairs(listfiles("aris/CustomModules")) do 
+	if commit then
+		if isfolder("Aris") then 
+			if ((not isfile("Aris/commithash.txt")) or (readfile("Aris/commithash.txt") ~= commit or commit == "main")) then
+				for i,v in pairs({"Aris/Universal.lua", "Aris/MainScript.lua", "Aris/GuiLibrary.lua"}) do 
 					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
 						delfile(v)
 					end 
 				end
-			end
-			if isfolder("aris/Libraries") then 
-				for i,v in pairs(listfiles("aris/Libraries")) do 
-					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
-						delfile(v)
-					end 
+				if isfolder("Aris/CustomModules") then 
+					for i,v in pairs(listfiles("Aris/CustomModules")) do 
+						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+							delfile(v)
+						end 
+					end
 				end
+				if isfolder("Aris/Libraries") then 
+					for i,v in pairs(listfiles("Aris/Libraries")) do 
+						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+							delfile(v)
+						end 
+					end
+				end
+				writefile("Aris/commithash.txt", commit)
 			end
-			writefile("aris/commithash.txt", commit)
+		else
+			makefolder("Aris")
+			writefile("Aris/commithash.txt", commit)
 		end
 	else
-		makefolder("aris")
-		writefile("aris/commithash.txt", commit)
+		displayErrorPopup("Failed to connect to github, please try using a VPN.")
+		error("Failed to connect to github, please try using a VPN.")
 	end
-else
-	displayErrorPopup("Failed to connect to github, please try using a VPN.")
-	error("Failed to connect to github, please try using a VPN.")
 end
+
