a9f96592ec165c4b9cedf9441e4c2d9647a1ddd6
JHKING112 <98388174+JHKING112@users.noreply.github.com>
2023-06-09 23:13:18 +0900

    a

--- ArisMainScript.lua
+++ ArisMainScript.lua
@@ -47,3 +47,45 @@ local function arisGithubRequest(scripturl)
 	end
 	return readfile("aris/"..scripturl)
 end
+
+
+local commit = "main"
+for i,v in pairs(game:HttpGet("https://github.com/7GrandDadPGN/VapeV4ForRoblox"):split("\n")) do 
+		if v:find("commit") and v:find("fragment") then 
+		local str = v:split("/")[5]
+		commit = str:sub(0, str:find('"') - 1)
+		break
+	end
+end
+if commit then
+	if isfolder("aris") then 
+		if ((not isfile("aris/commithash.txt")) or (readfile("vape/commithash.txt") ~= commit or commit == "main")) then
+			for i,v in pairs({"aris/Universal.lua", "aris/MainScript.lua", "aris/GuiLibrary.lua"}) do 
+				if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					delfile(v)
+				end 
+			end
+			if isfolder("aris/CustomModules") then 
+				for i,v in pairs(listfiles("aris/CustomModules")) do 
+					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+						delfile(v)
+					end 
+				end
+			end
+			if isfolder("aris/Libraries") then 
+				for i,v in pairs(listfiles("aris/Libraries")) do 
+					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+						delfile(v)
+					end 
+				end
+			end
+			writefile("aris/commithash.txt", commit)
+		end
+	else
+		makefolder("aris")
+		writefile("aris/commithash.txt", commit)
+	end
+else
+	displayErrorPopup("Failed to connect to github, please try using a VPN.")
+	error("Failed to connect to github, please try using a VPN.")
+end
